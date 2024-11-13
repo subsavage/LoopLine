bdc5caeaf7600ecf18ca140a0e51efc2f01a3b33
Harshit Srivastava <69175270+subsavage@users.noreply.github.com>
2024-11-13 11:34:33 +0530

    add music

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:loop_line/music/music.dart';
 import 'package:loop_line/pages/game_screen.dart';
 import 'package:loop_line/pages/homepage.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+  final AudioController audioController = AudioController();
 
   @override
   Widget build(BuildContext context) {
+    audioController.initialize().then((_) {
+      audioController.startMusic();
+    });
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'LoopLine',
@@ -18,7 +23,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: HomePage(
+        audioController: audioController,
+      ),
     );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:loop_line/music/music.dart'; // Assuming AudioController is here
 import 'package:loop_line/pages/game_screen.dart';
 import 'package:loop_line/pages/help.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  final AudioController audioController;
+
+  const HomePage({super.key, required this.audioController});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isMusicPlaying = true; // Keeps track of music state
+
+  @override
+  void initState() {
+    super.initState();
+    // Start the music when the homepage loads
+    widget.audioController.startMusic();
+  }
+
+  @override
+  void dispose() {
+    // Stop the music when navigating away from homepage
+    widget.audioController.fadeOutMusic();
+    super.dispose();
+  }
+
+  void toggleMusic() {
+    setState(() {
+      isMusicPlaying = !isMusicPlaying;
+      if (isMusicPlaying) {
+        widget.audioController.startMusic();
+      } else {
+        widget.audioController.stopMusic();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,6 +97,28 @@ class HomePage extends StatelessWidget {
                     fontSize: 24,
                   ),
                 ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Music : ",
+                    style: TextStyle(
+                      fontFamily: "SourGummy",
+                      fontWeight: FontWeight.w600,
+                      fontSize: 18,
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      isMusicPlaying ? Icons.volume_up : Icons.volume_off,
+                      size: 24,
+                    ),
+                    onPressed: toggleMusic,
+                  ),
+                ],
               ),
             ],
           ),
